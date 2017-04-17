1afe3b463cbdfce0ffdf3fbdd8475a7095317daa
Erik Pearson <eapearson@lbl.gov>
2017-04-17 16:19:07 -0700

    Update module names for kbase-ui compat

--- src/plugin/modules/databrowserWidget.js
+++ src/plugin/modules/databrowserWidget.js
@@ -1,60 +1,58 @@
-
-/*global
- define
- */
-/*jslint
- browser: true,
- white: true
- */
-
 define([
     'jquery',
     'promise',
-    'kb/common/dom',
-    'kb/common/html',
-    'kb/service/utils',
-    'kb/common/utils',
-    'kb/service/client/workspace',
+    'kb_common/dom',
+    'kb_common/html',
+    'kb_service/utils',
+    'kb_common/utils',
+    'kb_service/client/workspace',
     'datatables_bootstrap'
-],
-    function ($, Promise, DOM, html, APIUtils, Utils, WorkspaceClient) {
-        'use strict';
-        
-        function parseDate(date) {
-            var parts = date.split(/[-T:+]/);
-            if (parts.length !== 7) {
-                throw new Error('Invalid date string');
-            }
-            var year = parseInt(parts[0], 10),
-                month = parseInt(parts[1], 10),
-                day = parseInt(parts[2], 10),
-                hour = parseInt(parts[3], 10),
-                minute = parseInt(parts[4], 10),
-                seconds = parseInt(parts[5], 10),
-                offset = parseInt(parts[6], 10);
+], function(
+    $,
+    Promise,
+    DOM,
+    html,
+    APIUtils,
+    Utils,
+    WorkspaceClient
+) {
+    'use strict';
 
-            if (offset !== 0) {
-                throw new Error('Date is not UTC');
-            }
+    function parseDate(date) {
+        var parts = date.split(/[-T:+]/);
+        if (parts.length !== 7) {
+            throw new Error('Invalid date string');
+        }
+        var year = parseInt(parts[0], 10),
+            month = parseInt(parts[1], 10),
+            day = parseInt(parts[2], 10),
+            hour = parseInt(parts[3], 10),
+            minute = parseInt(parts[4], 10),
+            seconds = parseInt(parts[5], 10),
+            offset = parseInt(parts[6], 10);
 
-            return new Date(Date.UTC(year, month - 1, day, hour, seconds));
+        if (offset !== 0) {
+            throw new Error('Date is not UTC');
         }
 
-        var widget = function (config) {
-            var mount, container, runtime = config.runtime;
-            var workspaceClient = new WorkspaceClient(runtime.getConfig('services.workspace.url'), {
-                token: runtime.getService('session').getAuthToken()
-            });
+        return new Date(Date.UTC(year, month - 1, day, hour, seconds));
+    }
 
-            var workspaceObjects;
+    var widget = function(config) {
+        var mount, container, runtime = config.runtime;
+        var workspaceClient = new WorkspaceClient(runtime.getConfig('services.workspace.url'), {
+            token: runtime.getService('session').getAuthToken()
+        });
 
-            function render(data) {
-                var a = html.tag('a'),
-                    tableId = html.genId(),
-                    columns = ['Object Name', 'Module', 'Type', 'Version', 'Icon', 'Lineage', 'Narrative', 'Version', 'Last Modified'],
-                    /* TODO: the workspace client should make a type object for us, but kbaseTypes is new */
-                    rows = data.map(function (object) {
-                        var type = {
+        var workspaceObjects;
+
+        function render(data) {
+            var a = html.tag('a'),
+                tableId = html.genId(),
+                columns = ['Object Name', 'Module', 'Type', 'Version', 'Icon', 'Lineage', 'Narrative', 'Version', 'Last Modified'],
+                /* TODO: the workspace client should make a type object for us, but kbaseTypes is new */
+                rows = data.map(function(object) {
+                    var type = {
                             module: object.info.typeModule,
                             name: object.info.typeName,
                             version: {
@@ -63,146 +61,146 @@ define([
                             }
                         },
                         objectRef = object.info.wsid + '/' + object.info.id + '/' + object.info.version;
-                        return [
-                            a({href: '#dataview/' + objectRef}, object.info.name),
-                            type.module,
-                            a({href: '#spec/type/' + runtime.service('type').makeTypeId(type)}, type.name),
-                            runtime.service('type').makeVersion(type),
-                            runtime.service('type').getIcon({
-                                type: type,
-                                size: 'medium'
-                            }).html,
-                            a({href: '#taxontest/lineage/' + objectRef}, 'Lineage'),
-                            a({href: '/narrative/' + object.narrative.workspaceId + '/' + object.info.id}, object.narrative.name),
-                            object.info.version,
-                            parseDate(object.info.save_date).toLocaleString()
-                        ];
-                    });
+                    return [
+                        a({ href: '#dataview/' + objectRef }, object.info.name),
+                        type.module,
+                        a({ href: '#spec/type/' + runtime.service('type').makeTypeId(type) }, type.name),
+                        runtime.service('type').makeVersion(type),
+                        runtime.service('type').getIcon({
+                            type: type,
+                            size: 'medium'
+                        }).html,
+                        a({ href: '#taxontest/lineage/' + objectRef }, 'Lineage'),
+                        a({ href: '/narrative/' + object.narrative.workspaceId + '/' + object.info.id }, object.narrative.name),
+                        object.info.version,
+                        parseDate(object.info.save_date).toLocaleString()
+                    ];
+                });
 
-                return {
-                    content: html.makeTable({
-                        columns: columns,
-                        rows: rows,
-                        classes: ['table', 'table-striped'],
-                        id: tableId
-                    }),
-                    afterAttach: function () {
-                        $('#' + tableId).dataTable();
-                    }
-                };
-            }
+            return {
+                content: html.makeTable({
+                    columns: columns,
+                    rows: rows,
+                    classes: ['table', 'table-striped'],
+                    id: tableId
+                }),
+                afterAttach: function() {
+                    $('#' + tableId).dataTable();
+                }
+            };
+        }
 
-            function getData(params) {
+        function getData(params) {
 
-                return Promise.resolve(workspaceClient.list_workspace_info({
+            return Promise.resolve(workspaceClient.list_workspace_info({
                     showDeleted: 0,
                     excludeGlobal: 0
                         // owners: [runtime.getService('session').getUsername()]
                 }))
-                    .then(function (data) {
-                        var workspaceList = [],
-                            workspaceDb = {}, i, wsInfo;
-                        for (i = 0; i < data.length; i += 1) {
-                            wsInfo = APIUtils.workspace_metadata_to_object(data[i]);
+                .then(function(data) {
+                    var workspaceList = [],
+                        workspaceDb = {},
+                        i, wsInfo;
+                    for (i = 0; i < data.length; i += 1) {
+                        wsInfo = APIUtils.workspace_metadata_to_object(data[i]);
 
-                            //if (Narrative.isValid(wsInfo)) {
-                            workspaceList.push(wsInfo.id);
-                            workspaceDb[wsInfo.id] = wsInfo;
-                            //}
-                        }
+                        //if (Narrative.isValid(wsInfo)) {
+                        workspaceList.push(wsInfo.id);
+                        workspaceDb[wsInfo.id] = wsInfo;
+                        //}
+                    }
 
-                        // We should now have the list of recently active narratives.
-                        // Now we sort and limit the list.
-                        // Now get the workspace details.
-                        var listObjectsParams = {
-                            ids: workspaceList,
-                            includeMetadata: 1
-                        };
-                        if (params.type) {
-                            listObjectsParams.type = params.type;
-                        }
-                        return [workspaceDb, Promise.resolve(workspaceClient.list_objects(listObjectsParams))];
-                    })
-                    .spread(function (workspaceDb, data) {
-                        workspaceObjects = data.map(function (info) {
-                            var wsObjectInfo = APIUtils.object_info_to_object(info);
-                            return {
-                                info: wsObjectInfo,
-                                narrative: {
-                                    workspaceId: wsObjectInfo.wsid,
-                                    name: workspaceDb[wsObjectInfo.wsid].metadata.narrative_nice_name || 'not a narrative'
-                                }
-                            };
-                        });
-                        return workspaceObjects;
-                    });
-            }
-
-            function attach(node) {
-                return Promise.try(function () {
-                    mount = node;
-
-                    container = DOM.createElement('div');
-                    DOM.append(mount, container);
-                    DOM.setHTML(container, html.loading());
-                });
-            }
-
-            function start(params) {
-                return Promise.try(function () {
-                    // nothing really to do ...                    
-                });
-            }
-
-            function run(params) {
-                return Promise.try(function () {
-                    var div = html.tag('div');
-                    DOM.setHTML(container, 'Loading data ... ' + html.loading());
-                    return getData(params)
-                        .then(function (data) {
-                            var rendered = render(data);
-                            DOM.setHTML(container, rendered.content);
-                            if (rendered.afterAttach) {
-                                rendered.afterAttach();
+                    // We should now have the list of recently active narratives.
+                    // Now we sort and limit the list.
+                    // Now get the workspace details.
+                    var listObjectsParams = {
+                        ids: workspaceList,
+                        includeMetadata: 1
+                    };
+                    if (params.type) {
+                        listObjectsParams.type = params.type;
+                    }
+                    return [workspaceDb, Promise.resolve(workspaceClient.list_objects(listObjectsParams))];
+                })
+                .spread(function(workspaceDb, data) {
+                    workspaceObjects = data.map(function(info) {
+                        var wsObjectInfo = APIUtils.object_info_to_object(info);
+                        return {
+                            info: wsObjectInfo,
+                            narrative: {
+                                workspaceId: wsObjectInfo.wsid,
+                                name: workspaceDb[wsObjectInfo.wsid].metadata.narrative_nice_name || 'not a narrative'
                             }
-                        })
-                        .catch(function (err) {
-                            DOM.setHTML(container, html.makePanel({
-                                title: 'Error',
-                                content: div([
-                                    err.message
-                                ])
-                            }));
-
-                        })
+                        };
+                    });
+                    return workspaceObjects;
                 });
-            }
+        }
 
-            function stop() {
-                return Promise.try(function () {
-                    // nothing really to do ...
-                });
-            }
+        function attach(node) {
+            return Promise.try(function() {
+                mount = node;
 
-            function detach() {
-                return Promise.try(function () {
-                    mount.removeChild(container);
-                });
-            }
+                container = DOM.createElement('div');
+                DOM.append(mount, container);
+                DOM.setHTML(container, html.loading());
+            });
+        }
 
-            return {
-                attach: attach,
-                start: start,
-                run: run,
-                stop: stop,
-                detach: detach
-            };
-        };
+        function start(params) {
+            return Promise.try(function() {
+                // nothing really to do ...                    
+            });
+        }
+
+        function run(params) {
+            return Promise.try(function() {
+                var div = html.tag('div');
+                DOM.setHTML(container, 'Loading data ... ' + html.loading());
+                return getData(params)
+                    .then(function(data) {
+                        var rendered = render(data);
+                        DOM.setHTML(container, rendered.content);
+                        if (rendered.afterAttach) {
+                            rendered.afterAttach();
+                        }
+                    })
+                    .catch(function(err) {
+                        DOM.setHTML(container, html.makePanel({
+                            title: 'Error',
+                            content: div([
+                                err.message
+                            ])
+                        }));
+
+                    })
+            });
+        }
+
+        function stop() {
+            return Promise.try(function() {
+                // nothing really to do ...
+            });
+        }
+
+        function detach() {
+            return Promise.try(function() {
+                mount.removeChild(container);
+            });
+        }
 
         return {
-            make: function (config) {
-                return widget(config);
-            }
+            attach: attach,
+            start: start,
+            run: run,
+            stop: stop,
+            detach: detach
         };
-    }
-);
+    };
+
+    return {
+        make: function(config) {
+            return widget(config);
+        }
+    };
+});
--- src/plugin/modules/panel.js
+++ src/plugin/modules/panel.js
@@ -1,96 +1,95 @@
-/*global
- define, require
- */
-/*jslint
- white: true
- */
 define([
     'promise',
-    'kb/common/html',
-    'kb/common/dom',
-    'kb/widget/widgetSet'
-],
-    function (Promise, html, DOM, WidgetSet) {
-        'use strict';
+    'kb_common/html',
+    'kb_common/dom',
+    'kb_widget/widgetSet'
+], function(Promise, html, DOM, WidgetSet) {
+    'use strict';
 
-        function widget(config) {
-            var mount, container, runtime = config.runtime,
-                widgetSet = WidgetSet.make({runtime: runtime}),
-                div = html.tag('div');
+    function widget(config) {
+        var mount, container, runtime = config.runtime,
+            widgetSet = WidgetSet.make({ runtime: runtime }),
+            div = html.tag('div');
 
-            function render() {
-                return {
-                    title: 'Data Browser',
-                    content: div({class: 'kb-panel-databrowser container-fluid'}, [
-                        div({class: 'row'}, [
-                            div({class: 'col-md-12'}, [
-                                html.bsPanel('Data Browser Widget', div({id: widgetSet.addWidget('databrowser_widget')}))
-                            ])
+        function render() {
+            return {
+                title: 'Data Browser',
+                content: div({ class: 'kb-panel-databrowser container-fluid' }, [
+                    div({ class: 'row' }, [
+                        div({ class: 'col-md-12' }, [
+                            html.bsPanel('Data Browser Widget', div({ id: widgetSet.addWidget('databrowser_widget') }))
                         ])
                     ])
-                };
-            }
-
-            var rendered = render();
-
-            // Widget API
-            function init() {
-                return Promise.try(function () {
-                    return widgetSet.init(config);
-                });
-            }
-            function attach(node) {
-                return Promise.try(function () {
-                    mount = node;
-                    container = DOM.createElement('div');
-                    mount.appendChild(container);
-                    container.innerHTML = html.flatten(rendered.content);
-                    runtime.send('ui', 'setTitle', rendered.title);
-                    return widgetSet.attach(node);
-                });
-            }
-            function detach() {
-                return Promise.try(function () {
-                    mount.removeChild(container);
-                    container = null;
-                    return widgetSet.detach();
-                });
-            }
-            function start(params) {
-                return Promise.try(function () {
-                    return widgetSet.start(params);
-                });
-            }
-            function run(params) {
-                return Promise.try(function () {
-                    return widgetSet.run(params);
-                });
-            }
-            function stop() {
-                return Promise.try(function () {
-                    return widgetSet.stop();
-                });
-            }
-            function destroy() {
-                return Promise.try(function () {
-                    return widgetSet.destroy();
-                });
-            }
-
-            return {
-                init: init,
-                attach: attach,
-                detach: detach,
-                start: start,
-                run: run,
-                stop: stop,
-                destroy: destroy
+                ])
             };
         }
 
+        var rendered = render();
+
+        // Widget API
+        function init() {
+            return Promise.try(function() {
+                return widgetSet.init(config);
+            });
+        }
+
+        function attach(node) {
+            return Promise.try(function() {
+                mount = node;
+                container = DOM.createElement('div');
+                mount.appendChild(container);
+                container.innerHTML = html.flatten(rendered.content);
+                runtime.send('ui', 'setTitle', rendered.title);
+                return widgetSet.attach(node);
+            });
+        }
+
+        function detach() {
+            return Promise.try(function() {
+                mount.removeChild(container);
+                container = null;
+                return widgetSet.detach();
+            });
+        }
+
+        function start(params) {
+            return Promise.try(function() {
+                return widgetSet.start(params);
+            });
+        }
+
+        function run(params) {
+            return Promise.try(function() {
+                return widgetSet.run(params);
+            });
+        }
+
+        function stop() {
+            return Promise.try(function() {
+                return widgetSet.stop();
+            });
+        }
+
+        function destroy() {
+            return Promise.try(function() {
+                return widgetSet.destroy();
+            });
+        }
+
         return {
-            make: function (config) {
-                return widget(config);
-            }
+            init: init,
+            attach: attach,
+            detach: detach,
+            start: start,
+            run: run,
+            stop: stop,
+            destroy: destroy
         };
-    });
+    }
+
+    return {
+        make: function(config) {
+            return widget(config);
+        }
+    };
+});
